208bf429ed0f94637e52a0f182f432d08654b5e8
Conrad451 <67305888+Conrad451@users.noreply.github.com>
2025-10-04 15:53:05 -0500

    submit

--- DiceRoll.py
+++ DiceRoll.py
@@ -1,18 +1,25 @@
 #DiceRoll.py
-#Name:
-#Date:
-#Assignment:
+#Name:Connor Pell
+#Date: 10/4
+#Assignment: Lab 6
 import random
 
 def main():
   #Create an empty list with possible roll values
   rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
-  #Create two dice values ranging from 1 - 6 each
-  
-  #find the sum total of the two dice
-  
-  #print statictics for dice rolls
+  for r in range(10000):#Create two dice values ranging from 1 - 6 each
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
 
+    
+    #find the sum total of the two dice
+    sumtotal = dice1 + dice2
+    rolls[sumtotal - 1] = rolls[sumtotal - 1 ] + 1
+    #print statictics for dice rolls
 
+  dice = 1
+  for count in rolls:
+    print(dice, ":", count)
+    dice += 1
 if __name__ == '__main__':
   main()
--- WordCount.py
+++ WordCount.py
@@ -4,11 +4,30 @@
 #Assignment:
 
 def main():
-  textFile = open("gettysberg.txt", 'r')
-  
+  while True:
+    filename = input("Enter a File Name: ")
+    try:
+      textFile = open(filename, 'r')
+      break
+    except FileNotFoundError:
+      print(f"File {filename} not found.")
+
+
+  lineCount = 0
+  wordCount = 0
+  letterCount = 0
   for line in textFile:
-    print(line)
-  
+    lineCount = lineCount +1
+    words = line.split()
+    for w in words:
+      wordCount = wordCount + 1
+      letters = len(w)
+      letterCount += letters
+    #print(line)
+  print()
+  print("Lines: " + str(lineCount))
+  print("Words: " + str(wordCount))
+  print("Characters: " + str(letterCount))
 
 if __name__ == '__main__':
   main()
